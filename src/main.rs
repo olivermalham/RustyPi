5de61a1c864e5f1d5c512ea6888ba221fb316ea9
olivermalham <oliver.malham@gmail.com>
2022-12-22 21:56:04 +0000

    Generating basic image, not sending to client yet

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,29 @@
-#![feature(proc_macro_hygiene, decl_macro)]
-
 #[macro_use] extern crate rocket;
+mod gen_image;
+
+extern crate image;
+extern crate num_complex;
+
+use std::io::Cursor;
+use crate::gen_image::generate;
 
 #[get("/")]
 fn index() -> &'static str {
-    "RustyPi Running!"
+    let imgx = 800;
+    let imgy = 800;
+
+    let imgbuf = generate(imgx, imgy);
+    // Save the image as “fractal.png”, the format is deduced from the path
+    // imgbuf.save("fractal.png").unwrap();
+    // let mut bytes = Vec::new();
+    let mut bytes: Cursor<Vec<u8>> = Cursor::new(Vec::new());
+    imgbuf.write_to(&mut bytes, image::ImageOutputFormat::from(image::ImageFormat::Jpeg)).expect("");
+
+    // TODO: How can I return the image to the browser?
+    "Hello, world!"
 }
 
-fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
