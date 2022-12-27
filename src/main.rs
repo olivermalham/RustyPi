4e91466487cc4d5dd87c3a95eed8aec2d7360d11
olivermalham <oliver.malham@gmail.com>
2022-12-27 21:47:34 +0000

    Refactored to use Actix-web instead of Rocket, to see if there was any immediate speed benefit. Nope, but from what I've read it is the better maintained framework, so will persevere with it for now.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-#[macro_use] extern crate rocket;
 mod gen_image;
 
 extern crate image;
@@ -6,27 +5,41 @@ extern crate num_complex;
 
 use std::io::Cursor;
 use crate::gen_image::generate;
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::http::header::ContentType;
 
-use rocket::http::ContentType;
 
-
-#[get("/")]
-fn index() -> (ContentType, Vec<u8>) {
+fn image() -> Vec<u8> {
     let imgx = 1800;
     let imgy = 800;
 
-    let mut imgbuf = generate(imgx, imgy);
+    let imgbuf = generate(imgx, imgy);
 
-    for n in 1..100 {
-        imgbuf = generate(imgx, imgy);
-    }
+    // for n in 1..100 {
+    //     imgbuf = generate(imgx, imgy);
+    // }
+
     let mut bytes: Cursor<Vec<u8>> = Cursor::new(Vec::new());
-    imgbuf.write_to(&mut bytes, image::ImageOutputFormat::from(image::ImageFormat::Jpeg)).expect("");
+    imgbuf.write_to(&mut bytes,
+                    image::ImageOutputFormat::from(image::ImageFormat::Jpeg)
+                    ).expect("");
 
-    return (ContentType::JPEG, bytes.into_inner());
+    bytes.into_inner()
 }
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().content_type(ContentType::jpeg()).body(image())
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+    })
+    .bind(("127.0.0.1", 8000))?
+    .run()
+    .await
 }
